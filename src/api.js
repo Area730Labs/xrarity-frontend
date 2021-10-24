ef97e806d45abb4a839d9285f73a62f9b70c48b9
Vlad M <webmonch@gmail.com>
2021-10-24 23:46:09 +0300

    upd favicon

--- src/api.js
+++ src/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
   // baseURL: `https://api.soltracker.io/`
-  baseURL: `http://127.0.0.1:8000/`,
+  baseURL: ``,
   // baseURL: `https://soltracker-api-test.herokuapp.com/`,
 });
 
--- src/components/Header.js
+++ src/components/Header.js
@@ -52,7 +52,7 @@ class Header extends React.Component {
         <header className="d-flex flex-wrap align-items-center justify-content-center justify-content-md-between py-1 mb-2 border-bottom">
             <a href="/" className="d-flex align-items-center col-md-3 mb-2 mb-md-0 text-dark text-decoration-none">
                 <img className="bi me-2" src={require('../img/logo.png').default} width="30px" height="30px" />
-                <span className="fs-5"><strong>SOL Tracker</strong></span>
+                <span className="fs-5"><strong>X Rarity</strong></span>
             </a>
     
             <ul className="nav col-12 col-md-auto mb-2 justify-content-center mb-md-0">
